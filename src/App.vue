6322200631fb850bf09eb75a272ca9f9eb586b4a
tianxuanzhizi <286129936@qq.com>
2021-12-09 15:38:15 +0800

    perf: upgrdae butterfly

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <div id="app">
-    <router-view />
+    <home></home>
   </div>
 </template>
 
+<script>
+export default {
+  components: {
+    home: () => import("@/views/Home.vue")
+  }
+};
+</script>
 <style lang="less">
 html,
 body,
